d88c953667b9860d2ef2b4e26fb0ec5f3e2fbe77
hragavan <hragavan71@gmail.com>
2013-09-14 13:23:51 -0700

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,17 +2,17 @@
 
 class Login extends CI_Controller {
 
- function __construct()
- {
-   parent::__construct();
- }
+ 	function __construct()
+ 	{
+		parent::__construct();
+ 	}
 
- function index()
- {
-	 $this->load->helper('url');
-   $this->load->helper(array('form'));
-   $this->load->view('login_view');
- }
+	function index()
+ 	{
+		$this->load->helper('url');
+   		$this->load->helper(array('form'));
+   		$this->load->view('login_view');
+ 	}
 
 }
 
