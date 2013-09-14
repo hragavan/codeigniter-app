5d502c8e77e400605e365acdc4e8190d79660a07
hragavan <hragavan71@gmail.com>
2013-09-14 14:30:07 -0700

    Test

--- application/controllers/NewRegister.php
+++ application/controllers/NewRegister.php
@@ -2,7 +2,6 @@
 
 class NewRegister extends CI_Controller {
 
-<<<<<<< HEAD
     function __construct() {
         parent::__construct();
         $this->load->model('register', '', TRUE);
@@ -25,30 +24,5 @@ class NewRegister extends CI_Controller {
         }
     }
 
-=======
- 	function __construct()
- 	{
-		 parent::__construct();
-		 $this->load->model('register','',TRUE);
- 	}
- 	function index()
- 	{
-		//This method will have the credentials validation
-   		$this->load->library('form_validation');
-   		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
-   		$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
-   		$data['username']=$this->input->post("username");
-    		$data['password']=MD5($this->input->post("password"));
-   		$data['firstname']=$this->input->post("firstname");
-		$data['lastname']=$this->input->post("lastname");
-   		$data['emailID']=$this->input->post("email");
-  
-		$result=$this->register->add($data); 
-   		if($result==true)
-   		{
-		   $this->load->view('welcome_message');
-   		}
- 	}
->>>>>>> c71e86ff3342d22540806188a055f5b00419f322
 }
 ?>
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -5,7 +5,6 @@ if (!defined('BASEPATH'))
 
 class Login extends CI_Controller {
 
-<<<<<<< HEAD
     function __construct() {
         parent::__construct();
     }
@@ -15,19 +14,6 @@ class Login extends CI_Controller {
         $this->load->helper(array('form'));
         $this->load->view('login_view');
     }
-=======
- 	function __construct()
- 	{
-		parent::__construct();
- 	}
-
-	function index()
- 	{
-		$this->load->helper('url');
-   		$this->load->helper(array('form'));
-   		$this->load->view('login_view');
- 	}
->>>>>>> c71e86ff3342d22540806188a055f5b00419f322
 
 }
 
