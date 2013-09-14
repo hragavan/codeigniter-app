b745dfcc9ab1c3d95680ebd3dbc5116298f3a490
hragavan <hragavan71@gmail.com>
2013-09-14 13:38:19 -0700

    Basic changes

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,18 +1,19 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
- function __construct()
- {
-   parent::__construct();
- }
+    function __construct() {
+        parent::__construct();
+    }
 
- function index()
- {
-	 $this->load->helper('url');
-   $this->load->helper(array('form'));
-   $this->load->view('login_view');
- }
+    function index() {
+        $this->load->helper('url');
+        $this->load->helper(array('form'));
+        $this->load->view('login_view');
+    }
 
 }
 
