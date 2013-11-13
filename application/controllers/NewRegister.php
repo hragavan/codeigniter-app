7ea6bc8e2a6e215c9cf1e95649740c5b4f9d985c
hragavan <hragavan71@gmail.com>
2013-11-12 18:01:36 -0800

    slider

--- application/controllers/NewRegister.php
+++ application/controllers/NewRegister.php
@@ -5,25 +5,39 @@ class NewRegister extends CI_Controller {
  {
    parent::__construct();
    $this->load->model('register','',TRUE);
+    $this->load->model('activity','',TRUE);
  }
  function index()
  {
    //This method will have the credentials validation
+   $this->load->helper(array('form', 'url'));
+
    $this->load->library('form_validation');
 
    $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
    $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
-   $data['username']=$this->input->post("username");
-    $data['password']=MD5($this->input->post("password"));
-   $data['firstname']=$this->input->post("firstname");
-   $data['lastname']=$this->input->post("lastname");
+   
+  if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('login_view.phtml');
+		}
+		else
+		{
+			$data['username']=$this->input->post("username");
+			$data['password']=MD5($this->input->post("password"));
+			$data['firstname']=$this->input->post("firstname");
+			$data['lastname']=$this->input->post("lastname");
    $data['emailID']=$this->input->post("email");
-  
-   $result=$this->register->add($data); 
+   $result=$this->register->add($data);
    if($result==true)
    {
-	   $this->load->view('welcome_message.phtml');
+	   $data['activity']=$this->activity->activity_list();	
+	   $this->load->view('welcome_message.phtml',$data);
    }
+			
+		}
+    
+  
  }
 }
 /*
